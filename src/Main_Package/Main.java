f8042d8b07abf8e96282060b53faa449873c646b
Mohab2000 <mohabahmed20005@gmail.com>
2023-10-18 12:17:34 +0300

    SINGLE LINKED LIST DONE

--- src/Main_Package/Main.java
+++ src/Main_Package/Main.java
@@ -9,16 +9,10 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.makeEmpty();
-        myLinkedList.append(1);
         myLinkedList.append(2);
-
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
-
-        System.out.println("\nLinked List:");
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.reverse();
         myLinkedList.printList();
-
     }
 }
